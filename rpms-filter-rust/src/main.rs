caf036b1cca1b8e68d151f4445f97426f493533a
Andy Georges <itkovian@gmail.com>
2017-11-17 14:04:22 +0100

    Using basenames for the sorting

--- rpms-filter-rust/src/main.rs
+++ rpms-filter-rust/src/main.rs
@@ -11,9 +11,8 @@ use std::path::Path;
 
 /// The options for the rpms filter
 ///
-///     - path: can take multiple value,
-///     metadata: s
-///     - log: boolean indicating if events should be logged
+///     - locked: path to file with list of locked packages
+///     - remove: path to file with removal candidates' absolute path
 fn get_opts<'a>() -> ArgMatches<'a> {
     App::new("RPM filter")
         .version(crate_version!())
@@ -36,40 +35,43 @@ fn get_opts<'a>() -> ArgMatches<'a> {
 
 
 fn regexify<'a>(pattern: &'a str) -> (&'a str, Regex) {
-
     let prefix = pattern.split("*").nth(0).unwrap();
-    pattern.replace("*", "[-a-zA-Z0-9_.]*");
-    let regex = Regex::new(pattern).unwrap();
-
+    let regex_pattern = pattern.replace("*", "[-a-zA-Z0-9_.]*");
+    let regex = Regex::new(&regex_pattern.as_str()).unwrap();
     (prefix, regex)
 }
 
 
-fn remove<'a>(locked_packages: Vec<&str>, removal_candidates: Vec<&'a str>) -> Vec<&'a str> {
+fn remove<'a>(locked_packages: Vec<&str>, removal_candidates: Vec<(&'a str, &'a str)>) -> Vec<&'a str> {
 
     let mut removals : Vec<&str> = Vec::new();
-    let mut candidates : Vec<&str> = removal_candidates.clone();
+    let mut candidates : Vec<(&str, &str)> = removal_candidates.clone();
 
     for lp in locked_packages.iter() {
+        //println!("Checking locked package {}", &lp);
 
         let (prefix, regex) : (&str, Regex) = regexify(lp);
+        //println!("----> prefix: {}, regex: {:?}", &prefix, &regex);
 
         // these can never be matched, since they come before the prefix
         let mut rs : Vec<&str> = candidates.iter()
-                                       .cloned()
-                                       .take_while(|s| s < &prefix).collect();
+                                           .take_while(|&&(bn , _)| bn < &prefix)
+                                           .map(|&(_, p)| p)
+                                           .collect();
+        //println!("----> Removing {:?}", &rs);
         removals.append(&mut rs);
 
         // we drop the ones matching the current regex, since these are locked
         candidates = candidates.iter()
+                               .skip_while(|&&(bn, _)| bn < &prefix)
+                               .skip_while(|&&(bn, _)| regex.is_match(bn))
                                .cloned()
-                               .skip_while(|s| s < &prefix)
-                               .skip_while(|s| regex.is_match(s))  // CHECK: we have sorted values, so the first non match should break ?
                                .collect();
+        //println!("----> {} candidates left", candidates.len());
     }
-
     removals
 }
+
 
 // Return the lines in a file as an iterator
 fn get_lines<P>(path: P) -> Result< Vec<String>, io::Error>
@@ -78,11 +80,10 @@ where
 {
     let mut file = File::open(path).unwrap();
     let mut buffer = String::new();
-
     file.read_to_string(&mut buffer).unwrap();
-
     Ok(buffer.lines().map(ToOwned::to_owned).collect())
 }
+
 
 fn main() {
     let opts = get_opts();
@@ -91,14 +92,24 @@ fn main() {
     let mut locked_packages: Vec<&str> = locked_lines.iter()
                                                      .map(|s| s.split(',').nth(0).unwrap())
                                                      .collect();
-
     locked_packages.sort();
 
     let removal_lines : Vec<String> = get_lines(Path::new(opts.value_of("remove").unwrap())).unwrap();
-    let mut removal_candidates : Vec<&str> = removal_lines.iter().map(|s| s.as_str()).collect();
+    let removal_candidates_ : Vec<&str> = removal_lines.iter().map(|s| s.as_str()).collect();
+    let mut removal_candidates : Vec<(&str, &str)> = removal_candidates_.clone()
+        .iter()
+        .map(|&s| Path::new(s).file_name().unwrap().to_str().unwrap())
+        .zip(
+            removal_candidates_.clone()
+            .iter()
+            .map(|s| *s)
+        ).collect();
+
     removal_candidates.sort();
 
     let removals = remove(locked_packages, removal_candidates);
 
-    removals.iter().map(|s| print!("{}", s));
+    for p in removals.iter() {
+        println!("{}", p);
+    }
 }
